bb10245bcb181037ed808710b1d3bb8e6c729261
naeem <61241046+Naeemah-Khatib@users.noreply.github.com>
2021-09-23 19:36:38 +0530

    2nd method added

--- SpiralMatrix.java
+++ SpiralMatrix.java
@@ -33,3 +33,41 @@ public class Main
 	        System.out.print(arr[a][b]);
 	}
 }
+
+
+//Program to print any matrix in spiral form
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int m = s.nextInt();
+		int n = s.nextInt();
+		
+		int[][] arr = new int[m][n];
+		for(int i=0;i<m;i++){
+		    for(int j=0;j<n;j++)
+		        arr[i][j] = s.nextInt();
+		}
+		
+		int k=0, l=0;
+		while(k<m && l<n){
+		    for(int i=l;i<n;i++)
+		        System.out.print(arr[k][i]+" ");
+		    k++;
+		    for(int i=k;i<m;i++)
+		        System.out.print(arr[i][n-1]+" ");
+		    n--;
+		    if(k<m){
+		        for(int i=n-1;i>=l;i--)
+		            System.out.print(arr[m-1][i]+" ");
+		        m--;
+		    }
+		    if(l<n){
+		        for(int i=m-1;i>=k;i--)
+		            System.out.print(arr[i][l]+" ");
+		        l++;
+		    }
+		}
+	}
+}
