383d77e15757cb0c4a2b747f18cce4846b249994
naeem <61241046+Naeemah-Khatib@users.noreply.github.com>
2021-09-23 17:14:08 +0530

    Update SpiralMatrix.java

--- SpiralMatrix.java
+++ SpiralMatrix.java
@@ -6,7 +6,7 @@ public class Main
 		Scanner s = new Scanner(System.in);
 		int n = s.nextInt();
 		int a = 0;
-		int b = n;
+		int b = n-1;
 		int[][] arr = new int[n][n];
 		for(int i=0;i<n;i++){
 		    for(int j=0;j<n;j++)
@@ -15,20 +15,21 @@ public class Main
 		int r,c;
 		while(a<b){
 		    r = a;
-    		for(c=a;c<b;c++)
+    		for(c=a;c<=b;c++)
     		    System.out.print(arr[r][c]+" ");
-    		c=b-1;
-    		for(r=a+1;r<b;r++)
+    		c=b;
+    		for(r=a+1;r<=b;r++)
     		    System.out.print(arr[r][c]+" ");
-    		r = b-1;
-    		for(c=b-2;c>=a;c--)
+    		r = b;
+    		for(c=b-1;c>=a;c--)
     		    System.out.print(arr[r][c]+" ");
     		c = a;
-    		for(r=b-2;r>a;r--)
+    		for(r=b-1;r>a;r--)
     		    System.out.print(arr[r][c]+" ");
     		a++; b--;
+    	//	System.out.println("***"+a+" "+b+"***");
 	    }
-	    if(n%2!=0 && a==b)
-	        System.out.print(arr[a][a]);
+	    if(a==b)
+	        System.out.print(arr[a][b]);
 	}
 }
