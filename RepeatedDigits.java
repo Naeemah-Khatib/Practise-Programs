8917cd9dc25e0aac4f9a9aa3561fba1ae9378bdd
naeem <61241046+Naeemah-Khatib@users.noreply.github.com>
2021-10-06 09:54:50 +0530

    2nd method added

--- RepeatedDigits.java
+++ RepeatedDigits.java
@@ -26,3 +26,26 @@ public class Main
         System.out.println(count);
 	}
 }
+
+
+// 2nd method - use Set
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n1 = s.nextInt();
+		int n2 = s.nextInt();
+		int count=0;
+		for(int i=n1;i<=n2;i++){
+		    int num=i;
+		    TreeSet<Integer> set = new TreeSet<Integer>();
+		    while(num>0){
+		        set.add(num%10);
+		        num /= 10;
+		    }
+		    if(set.size() == String.valueOf(i).length()) count++;
+		}
+		System.out.print(count);
+	}
+}
